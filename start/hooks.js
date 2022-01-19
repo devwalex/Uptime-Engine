681d7eb47e97b7814d895ff4143a09627ce59b26
“devwalex” <salamiusmanolawale@gmail.com>
2022-01-19 16:59:54 +0100

    feat: add subscription jobs

--- start/hooks.js
+++ start/hooks.js
@@ -1,5 +1,8 @@
 const { hooks } = require('@adonisjs/ignitor');
 
+function convertDateInMillisecondToDay(date) {
+  return date / (24 * 60 * 60 * 1000);
+}
 hooks.after.providersRegistered(async () => {
   const Validator = use('Validator');
   const Database = use('Database');
@@ -47,3 +50,64 @@ hooks.after.providersRegistered(async () => {
   Validator.extend('exists', existsFn);
   Validator.extend('checkDomainOrIp', checkDomainOrIP);
 });
+
+hooks.after.httpServer(async () => {
+
+  // const Bull = use('Rocketseat/Bull');
+  // const Job = use('App/Jobs/PingDomain');
+  // const Domain = use('App/Models/Domain');
+  const Subscription = use('App/Models/Subscription');
+  const Billing = use('App/Models/Billing');
+  var paystack = require('paystack-api')('sk_test_050b26c2a17145b5e696cf824ef7ea8aecafb240');
+  
+  console.log('Started Hook');
+    const schedule = require('node-schedule');
+    const count = 0;
+
+    schedule.scheduleJob("* * * * *", async function () {
+    try {
+  
+      const subscriptions = await Subscription.query().where('trial_period', '>' ,  0).where('is_deleted', false).fetch()
+  
+      let expired = [];
+      subscriptions.toJSON().forEach(async (subscription) => {
+        console.log('Date', convertDateInMillisecondToDay(Date.now() - Date.parse(subscription.created_at)));
+        console.log('Days left', convertDateInMillisecondToDay(subscription.trial_period - (Date.now() - Date.parse(subscription.created_at))));
+        if (convertDateInMillisecondToDay(Date.now() - Date.parse(subscription.created_at)) >= subscription.trial_period) {
+          expired.push(subscription.user_id);
+        }
+      });
+      
+      console.log('expired', expired);
+  
+      if (expired.length > 0) {
+        await Subscription.query().whereIn('user_id', expired).update({trial_period: 0})
+    
+        const billings = await Billing.query().whereIn('user_id', expired).where('is_active', true).fetch()
+    
+        console.log('billing', billings.toJSON());
+    
+        for (const billing of billings.toJSON()) {
+          const subscription = await Subscription.query().where('user_id', billing.user_id).where('is_deleted', false).where('is_active', false).first()
+          const processSubscription = await paystack.subscription.create({
+            customer: billing.customer_code,
+            plan: subscription.plan_code,
+            authorization: JSON.parse(billing.authorization)['authorization_code']
+          })
+          console.log('processSubscription>>', processSubscription);
+
+          // subscription.is_active = true;
+          // subscription.start_date = Date.now()
+          // await subscription.save()
+    
+        }
+  
+      }
+    } catch (error) {
+      console.log('Error', error.error);
+    }
+  
+  
+    });
+  
+  });
