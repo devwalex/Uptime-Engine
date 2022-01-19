765557c8f964e52f3ccf439639ebde55975d8f4b
“devwalex” <salamiusmanolawale@gmail.com>
2022-01-19 17:00:25 +0100

    chore: update

--- app/Controllers/Http/Billing/SubscriptionController.js
+++ app/Controllers/Http/Billing/SubscriptionController.js
@@ -59,55 +59,6 @@ class SubscriptionController {
     }
   }
 
-  async verifyCard({ request, response, auth }) {
-    try {
-      const authenticatedUser = await auth.current.user;
-      const { reference } = request.post()
-
-      const verify = await paystack.transaction.verify({ reference })
-
-      console.log('verif=========================', verify);
-
-      const billing = await Billing.query().where('user_id', authenticatedUser.id).first()
-      if (!billing.is_verified) {
-
-        if (verify.data.status === 'success') {
-          billing.email = verify.data.customer.email
-          billing.customer_code = verify.data.customer.customer_code
-          billing.authorization = JSON.stringify(verify.data.authorization)
-          billing.is_active = true
-          billing.is_verified = true
-          await billing.save()
-
-          // Refund 
-          await paystack.refund.create({ transaction: verify.data.id, amount: verify.data.amount })
-
-        } else {
-          return response.status(400).json({
-            status: 'Bad Request',
-            message: 'Failed to verify card.',
-            status_code: 400,
-          });
-        }
-      }
-
-      return response.status(201).json({
-        status: 'Success',
-        message: 'Card Verified Successfully.',
-        status_code: 201,
-      });
-
-    } catch (error) {
-      console.error('tokenizeCard Error ==>', error);
-      return response.status(500).json({
-        status: 'Internal Server Error',
-        message: 'An unexpected error occurred',
-        status_code: 500,
-        error,
-      });
-    }
-  }
-
   async subscribe({ request, response, auth }) {
     try {
       // const { plan_code } = request.post()
@@ -133,43 +84,6 @@ class SubscriptionController {
         message: 'Authorization URL created.',
         status_code: 201,
         results: processSubscription.data
-      });
-
-    } catch (error) {
-      console.error('Subscribe Error ==>', error);
-      return response.status(500).json({
-        status: 'Internal Server Error',
-        message: 'An unexpected error occurred',
-        status_code: 500,
-        error,
-      });
-    }
-  }
-
-  async verifySubscription({ request, response }) {
-    try {
-
-      //validate event
-      // var hash = crypto.createHmac('sha512', secret).update(JSON.stringify(request.body)).digest('hex');
-      // if (hash === request.headers['x-paystack-signature']) {
-      // // Retrieve the request's body
-      // var event = request.body;
-      // // Do something with event  
-      // console.log('event', event);
-      // }
-
-      var result = request.body;
-
-      if (result.event === 'subscription.create') {
-
-
-
-      }
-      return response.status(201).json({
-        status: 'Success',
-        message: 'Verify Subscription Successful.',
-        status_code: 201,
-        results: event
       });
 
     } catch (error) {
